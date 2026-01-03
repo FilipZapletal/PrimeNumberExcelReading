c190145ff8be485f547bf5208e24c25ab1b6a6ae
FilipZapletal <f.zapletal0@seznam.cz>
2026-01-03 16:40:18 +0100

    Refactoring the structure of the FileRead class so the methods are separate

--- src/main/java/PrimeNumber/FileRead.java
+++ src/main/java/PrimeNumber/FileRead.java
@@ -12,6 +12,54 @@ import java.io.IOException;
 public class FileRead {
     private static final Logger logger = LoggerFactory.getLogger(FileRead.class);
 
+    public static void processFile(String filePath) throws IOException {
+        File file = new File(filePath);
+        FileInputStream fis = new FileInputStream(file);
+
+        Workbook workbook = new XSSFWorkbook(fis);
+
+        Sheet List1 = workbook.getSheetAt(0);
+
+        int last = List1.getLastRowNum();
+
+        for (int r = 0; r <= last; r++) {
+            Row row = List1.getRow(r);
+            if (row == null) {
+                continue;
+            }
+            processRow(row);
+        }
+    }
+
+    public static void processRow(Row row) {
+        Cell cellb = row.getCell(1);
+
+        if (cellb == null) {
+            return;
+        }
+
+        if (cellb.getCellType() == CellType.NUMERIC){
+            int value = (int) cellb.getNumericCellValue();
+            if (isPrime(value)) {
+                logger.info(String.valueOf(value));
+            }
+        }
+
+        if (cellb.getCellType() == CellType.STRING){
+            try {
+                int value = Integer.parseInt(cellb.getStringCellValue().trim());
+                if (isPrime(value)) {
+                    logger.info(String.valueOf(value));
+                }
+            } catch (NumberFormatException e) {
+                // případné kontrolní hlášení pro nečíselné hodnoty
+                //logger.info("Nelze konvertovat na integer (data nejsou číselné): {}", cellb.getStringCellValue());
+            }
+
+        }
+
+    }
+
     public static boolean isPrime(int number){
         if (number <= 1) {
             return false;
@@ -25,50 +73,15 @@ public class FileRead {
     }
 
     public static void main(String[] args) throws IOException {
-        String filePath = ".\\vzorek_dat.xlsx";
-        File file = new File(filePath);
-        FileInputStream fis = new FileInputStream(file);
+        String filePath;
 
-        Workbook workbook = new XSSFWorkbook(fis);
-
-        Sheet List1 = workbook.getSheet("List1");
-
-        int last = List1.getLastRowNum();
-
-        for (int r = 0; r <= last; r++) {
-            Row row = List1.getRow(r);
-            if (row == null) {
-                continue;
-            }
-
-
-            Cell cellb = row.getCell(1);
-
-            if (cellb == null) {
-                continue;
-            }
-
-            if (cellb.getCellType() == CellType.NUMERIC){
-                int value = (int) cellb.getNumericCellValue();
-                if (isPrime(value)) {
-                    logger.info(String.valueOf(value));
-                }
-            }
-
-            if (cellb.getCellType() == CellType.STRING){
-                try {
-                    int value = Integer.parseInt(cellb.getStringCellValue().trim());
-                    if (isPrime(value)) {
-                        logger.info(String.valueOf(value));
-                    }
-                } catch (NumberFormatException e) {
-                    logger.info("Nelze konvertovat na integer: " + cellb.getStringCellValue());
-                    continue;
-                }
-
-            }
-
+        if (args.length > 0) {
+            filePath = args[0];
+        } else {
+            filePath = ".\\vzorek_dat.xlsx";
         }
+
+        processFile(filePath);
 
 
     }
