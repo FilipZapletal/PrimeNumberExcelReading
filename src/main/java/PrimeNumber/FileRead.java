31a4b935de286b6f4d4f78cf2e8f320eb20bce84
FilipZapletal <f.zapletal0@seznam.cz>
2026-01-04 16:41:32 +0100

    moved main clas into Main clas :), added plugins for maven and jar commands in the command line

--- src/main/java/PrimeNumber/FileRead.java
+++ src/main/java/PrimeNumber/FileRead.java
@@ -72,17 +72,5 @@ public class FileRead {
         return true;
     }
 
-    public static void main(String[] args) throws IOException {
-        String filePath;
 
-        if (args.length > 0) {
-            filePath = args[0];
-        } else {
-            filePath = ".\\vzorek_dat.xlsx";
-        }
-
-        processFile(filePath);
-
-
-    }
 }
--- src/main/java/PrimeNumber/Main.java
+++ src/main/java/PrimeNumber/Main.java
@@ -1,7 +1,21 @@
 package PrimeNumber;
 
+import java.io.IOException;
+
+import static PrimeNumber.FileRead.processFile;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String filePath;
+
+        if (args.length > 0) {
+            filePath = args[0];
+        } else {
+            filePath = ".\\vzorek_dat.xlsx";
+        }
+
+        processFile(filePath);
+
 
     }
 }
